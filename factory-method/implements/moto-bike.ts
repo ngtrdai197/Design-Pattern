d58849786b88fe210da72386af065b3d8d98f1d6
Dai Nguyen <ngtrdai290197@gmail.com>
2021-07-03 17:41:29 +0700

    #1: Override mission method

--- factory-method/implements/moto-bike.ts
+++ factory-method/implements/moto-bike.ts
@@ -14,6 +14,7 @@ export class MotoBike extends Creator {
     return new MotoBikeModel();
   }
 
+  // override mission method
   public mission(): string {
     const product = this.factoryMethod();
     return `MotoBike -> Mission of product is ${product.operation()}`;
