43f2854ebd6d64cb7575aaeccbb7a6dcb7f70df1
Dai Nguyen <ngtrdai290197@gmail.com>
2021-07-12 10:16:09 +0700

    #4: Make constructor is private

--- creational/singleton/index.ts
+++ creational/singleton/index.ts
@@ -1,11 +1,11 @@
 class Singleton {
   private static instance: Singleton;
 
-  constructor() {}
+  private constructor() {}
 
   public static getInstance(): Singleton {
     if (!Singleton.instance) {
-      Singleton.instance = new Singleton();
+        Singleton.instance = new Singleton();
     }
     return Singleton.instance;
   }
@@ -14,14 +14,14 @@ class Singleton {
 }
 
 function clientCode() {
-    const s1 = Singleton.getInstance();
-    const s2 = Singleton.getInstance();
+  const s1 = Singleton.getInstance();
+  const s2 = Singleton.getInstance();
 
-    if (s1 === s2) {
-        console.log('Singleton works, both variables contain the same instance.');
-    } else {
-        console.log('Singleton failed, variables contain different instances.');
-    }
+  if (s1 === s2) {
+    console.log("Singleton works, both variables contain the same instance.");
+  } else {
+    console.log("Singleton failed, variables contain different instances.");
+  }
 }
 
 clientCode();
